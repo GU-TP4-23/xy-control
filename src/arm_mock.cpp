8b00868f21f0e7e27c148bedc5873f98910c1d17
Haroun <hash11223344@gmail.com>
2023-04-14 12:10:09 +0100

    changed the code in the loop into a function for teams to use

--- src/arm_mock.cpp
+++ src/arm_mock.cpp
@@ -4,43 +4,24 @@
 
 #define XY_ADDR 0x56
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.begin(9600);
-  while (!Serial);
-  digitalWrite(LED_BUILTIN, LOW);
-  
-  Serial.println("ARM SEGMENT MOCK-UP");
-
-  Serial.println("initialising I2C (Wire)...");
-  Serial.println("mode:\tmaster");
-  Wire.begin();
-  Serial.println("I2C initialised!");
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void XY_Coord_Sender(int x_coord, int y_coord)
+  {
   Serial.println("Encoding message...");
 
-  char header = 0x61;                          // header ('a')
-  float x = 123.456;                           // x value
-  float y = 789;                               // y value
+  char header = 0x61;                          // header ('a')                         
 
   Serial.print("Message header:\t");
   Serial.println(header);
 
-  char buf_x[sizeof(x)+1];                      // temp buffer to encode x to
-  memcpy(buf_x, &x, sizeof(x));                 // encode x value to temp buffer
+  char buf_x[sizeof(x_coord)+1];                      // temp buffer to encode x to
+  memcpy(buf_x, &x_coord, sizeof(x_coord));                 // encode x value to temp buffer
   Serial.print("Encoded X value of\t");
-  Serial.println(x);
+  Serial.println(x_coord);
 
-  char buf_y[sizeof(y)+1];                      // temp buffer to encode y to
-  memcpy(buf_y, &y, sizeof(y));                 // encode y value to temp buffer
+  char buf_y[sizeof(y_coord)+1];                      // temp buffer to encode y to
+  memcpy(buf_y, &y_coord, sizeof(y_coord));                 // encode y value to temp buffer
   Serial.print("Encoded Y value of\t");
-  Serial.println(y);
+  Serial.println(y_coord);
 
   Serial.print("Initialising transaction with slave device at ");
   Serial.print(XY_ADDR);
@@ -51,8 +32,8 @@ void loop() {
   Serial.println("I2C transaction initialised!");
   Serial.print("Buffering message:\t");
   int w_h = Wire.write(header);                 // write header to Wire buffer
-  int w_x = Wire.write(buf_x, sizeof(x));       // write encoded x buffer to Wire buffer
-  int w_y = Wire.write(buf_y, sizeof(y));       // write encoded y buffer to Wire buffer
+  int w_x = Wire.write(buf_x, sizeof(x_coord));       // write encoded x buffer to Wire buffer
+  int w_y = Wire.write(buf_y, sizeof(y_coord));       // write encoded y buffer to Wire buffer
   
   Serial.print("buffered ");
   Serial.print(w_h+w_x+w_y);
@@ -73,4 +54,26 @@ void loop() {
   Serial.println("Loop completed!");
 
   delay(10000);
+  }
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(9600);
+  while (!Serial);
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  Serial.println("ARM SEGMENT MOCK-UP");
+
+  Serial.println("initialising I2C (Wire)...");
+  Serial.println("mode:\tmaster");
+  Wire.begin();
+  Serial.println("I2C initialised!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  XY_Coord_Sender(10000,8000);
 }
